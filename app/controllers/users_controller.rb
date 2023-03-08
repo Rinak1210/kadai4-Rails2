ea85e50338032c5b313e37f2bf1192cda67db8d3
河原梨奈 <kawahararina@kawahararinanoMacBook.local>
2023-03-08 21:21:06 +0900

    Userモデルとusersコントローラーを生成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+#class UsersController < ApplicationController
+  #def edit
+    #user = User.find(params[:id])
+    #@image = user.image
+  #end
+
+  #def update
+    #if current_user.update(user_params)
+      #redirect_to user_path(current_user)
+    #else
+      #render :edit
+    #end
+  #end
+
+  #def show
+    #user = User.find(params[:id])
+    #@id = user.id
+    #@name = user.name
+    #@image = user.image
+    #@profile = user.profile
+    #@videos = user.videos.order("created_at DESC")
+  #end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :image, :profile)
+  end
+end
