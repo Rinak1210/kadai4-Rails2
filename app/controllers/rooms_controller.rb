d9be4a19749e10575d98356329cfa3276155b8a2
河原梨奈 <kawahararina@kawahararinanoMacBook.local>
2023-03-23 22:51:09 +0900

    roomのnew,create,ownアクションに取り掛かり中

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,9 +4,18 @@ class RoomsController < ApplicationController
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(params.require(:room).permit(:image, :name_of_hotel, :introduction, :price, :created_at))
+    if @room.save
+      flash[:success] = "施設が作成されました"
+      redirect_to :rooms
+      #上記は作成した施設詳細へリダイレクトに変更したい。rooms/id(数字)
+    else
+      render "new"
+    end
   end
 
   def show
