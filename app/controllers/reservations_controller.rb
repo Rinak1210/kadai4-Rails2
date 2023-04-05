837d54e06ab0804fc79b4b1685cdf358e2626cdf
河原梨奈 <kawahararina@kawahararinanoMacBook.local>
2023-04-05 21:34:49 +0900

    新規の予約機能のエラーを再度修正、「再予約」→確認画面はまだエラーが出ているので確認中

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,7 +2,7 @@ class ReservationsController < ApplicationController
   
   #予約済み一覧
   def index
-    @reservations=Reservation.all
+    @reservations= current_user.reservations.all
   end
 
   def create #room/showからの、予約確定ボタンの先
@@ -20,11 +20,11 @@ class ReservationsController < ApplicationController
   end
 
   def update
-    @reservation = Reservation.find(params[:id])
-    if @reservation.update((reservation_params))
-    redirect_to confirm_reservation_path
+    @reservation = Reservation.find(params[:reservation_id])
+    if @reservation.update(reservation_params)
+    redirect_to reservations_path
     else
-      render reservations_path
+      render confirm_reservations_path
     end
   end
 
@@ -35,7 +35,7 @@ class ReservationsController < ApplicationController
     redirect_to reservations_path
   end
 
-  #予約直前の内容の確認
+
   def confirm
     @room = Room.find(params[:reservation][:room_id])
     @checkin_date = params[:reservation][:checkin_date]
@@ -46,6 +46,28 @@ class ReservationsController < ApplicationController
     #confirm(予約内容確認)後、createへ飛ばす
     @reservation = Reservation.new
   end
+
+  # 予約直前の内容の確認
+  # controller(reservations conformアクション)
+#def confirm
+  # find_or_initialize_byはデータがあればとってくるなければ新しく初期化するというメソッド
+  #@reservation = current_user.reservations.find_or_initialize_by(id: params[:id])
+  # editとnewで分岐させる(すでに作成されているreservationはroomが紐づいているので...)
+  #if @reservation.room.present?
+    #room = @reservation.room
+  #else
+    #room = Room.find(reservation_params[:room_id])
+  #end
+
+  # edit newの共通の処理
+  #@reservation.number_of_people = reservation_params[:number_of_people].to_i
+  # 宿泊日数の計算
+  #length_of_stay = reservation_params[:checkout_date].to_date - reservation_params[:checkin_date].to_date
+  # 宿泊金額の合計計算
+  #amount_of_price = length_of_stay.to_i * room.price.to_i * @number_of_people.to_i
+  #@reservation.length_of_stay = length_of_stay
+  #@reservation.amount_of_price = amount_of_price
+#end
 
   private
 
